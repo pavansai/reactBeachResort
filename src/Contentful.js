00dc16c82aade7901523e7d15e9676976c291873
PAVAN SAI <82170111+pavansai@users.noreply.github.com>
2023-12-04 17:41:44 -0800

    Update Contentful.js
    hidden the api key

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,5 +1,5 @@
 import { createClient } from "contentful";
 export default createClient({
-    space:"ttygivntcvx0",
-    accessToken:"Bd6kSeLvyFJlgS0JHeBw_bROFxrr7v81X1NFRtMmw70"
+    space:"",
+    accessToken:""
 })
